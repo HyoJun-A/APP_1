25211c5bc6a9a7e890696a406bb5d4f54949f0ae
HyoJun-Ahn <wnsgy79@gmail.com>
2021-06-13 16:33:51 +0900

    SecondActivity.java

--- app/src/main/java/com/example/app_1/SecondActivity.java
+++ app/src/main/java/com/example/app_1/SecondActivity.java
@@ -1,9 +1,13 @@
 package com.example.app_1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
@@ -14,11 +18,41 @@ public class SecondActivity extends Activity {
         setContentView(R.layout.second);
 
         Button btnReturn = (Button) findViewById(R.id.btnReturn);
+        Button btnR = (Button) findViewById(R.id.btnPage);
+        EditText ed1 = (EditText) findViewById(R.id.edt10);
+        ImageView v1 = (ImageView) findViewById(R.id.vw);
+        ImageView v2 = (ImageView) findViewById(R.id.vw1);
+        ImageView v3 = (ImageView) findViewById(R.id.vw2);
+        
 
         btnReturn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        btnR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s3 = ed1.getText().toString();
+                float num = Float.parseFloat(s3);
+                if (num == 1){
+                    v1.setVisibility(View.VISIBLE);
+                    v2.setVisibility(View.INVISIBLE);
+                    v3.setVisibility(View.INVISIBLE);
+                }
+                else if (num == 2){
+                    v1.setVisibility(View.INVISIBLE);
+                    v2.setVisibility(View.VISIBLE);
+                    v3.setVisibility(View.INVISIBLE);
+                }
+                else if (num == 3){
+                    v1.setVisibility(View.INVISIBLE);
+                    v2.setVisibility(View.INVISIBLE);
+                    v3.setVisibility(View.VISIBLE);
+                }
+
             }
         });
     }
